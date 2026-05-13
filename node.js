fb55c2288a137b0c2f40d075b844b251af127b45
hosea-js <qrtkkpq49z@privaterelay.appleid.com>
2026-05-13 20:27:17 +0700

    some new feature

--- node.js
+++ node.js
@@ -3,9 +3,14 @@ const path = require('path');
 const app = express();
 
 // Serve a specific HTML file on the home route
+app.use(express.static(path.join(__dirname, 'index'))); // Serve static files from the 'index' directory
+
 app.get('/', (req, res) => {
-    app.use(express.static(path.join(__dirname, 'index'))); // Serve static files from the "public" folder
-    res.sendFile(path.join(__dirname, 'index', 'index.html')); // Send the specific HTML file
+    res.sendFile(path.join(__dirname, 'index', 'index.html')); // Send the index.html file for the home route
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'index', '404.html')); // Send the 404 HTML file for any unmatched routes
 });
 
 app.listen(3000, () => {
