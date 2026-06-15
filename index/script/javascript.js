a2f502760d2bb98d2d414265af152208efa856e0
hosea-js <qrtkkpq49z@privaterelay.appleid.com>
2026-06-15 16:58:18 +0700

    developing signin-up page

--- index/script/javascript.js
+++ index/script/javascript.js
@@ -7,3 +7,12 @@
         function closeSidebar() {
             sidebar.classList.remove('active');
         }
+
+        // Sign In/Up Head Title Changes
+        function showSignIn() {
+            document.title = "Sign In | SMPK Ignatius Slamet Riyadi E-Library";
+        }
+
+        function showSignUp() {
+            document.title = "Sign Up | SMPK Ignatius Slamet Riyadi E-Library";
+        }
