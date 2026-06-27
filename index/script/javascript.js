3529f03802c872af6f1bbecdba311a101e447224
hosea-js <qrtkkpq49z@privaterelay.appleid.com>
2026-06-27 09:50:52 +0700

    fix: Now, SIgn In and Up are working as expected. Backend will be added soon.

--- index/script/javascript.js
+++ index/script/javascript.js
@@ -15,3 +15,19 @@
 
         // Sign In/Up Form Toggle or Animation
         
+        function signUpBtn() {
+            updateTitle('Daftar');
+            console.log('Sign Up button clicked');
+            document.getElementById('signinForm').classList.add('hidden');
+            document.getElementById('signupForm').classList.add('active');
+        }
+
+        function signInBtn() {
+            updateTitle('Masuk');
+            console.log('Sign In button clicked');
+            document.getElementById('signupForm').classList.remove('active');
+            document.getElementById('signinForm').classList.remove('hidden');
+        }
+
+        forms.style.height = signinForm.offsetheight + 'px';
+        forms.style.height = signupForm.offsetheight + 'px';
